3fd4a622806e269dc5a95f280144a189d8998b29
Sathish V <sathish.v@msf.ad>
2022-06-24 16:54:52 +0530

    main

---
+++ src/main/java/kmb_task1/UsingJar.java
@@ -0,0 +1,10 @@
+package kmb_task1;
+import mysqlLog4j.Employee;
+public class UsingJar {
+
+	public static void main(String[] args) throws Exception { 
+		Employee obj = new Employee();
+		obj.InputData();
+	}
+
+}
